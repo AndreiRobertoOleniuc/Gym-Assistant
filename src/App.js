13a199a087121ba2c537446a2fd80e170c4261e9
AndreiRobertoOleniuc <andreioleniucroberto@gmail.com>
2021-06-28 20:51:44 +0200

    design login page done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState} from 'react';
 import Login from "./Login";
+import * as Font from "expo-font"
+import {AppLoading} from "expo";
+
+const getFonts = ()=> Font.loadAsync({
+    'nunito-regular':require(`../assets/fonts/Nunito-Regular.ttf`),
+    'nunito-bold':require(`../assets/fonts/Nunito-SemiBold.ttf`)
+});
 
 export default function App() {
-  return (
-    <Login/>
-  );
+  React.useEffect(()=>{
+    getFonts();
+  },[])
+  return <Login/>
 }
 
--- src/Login.js
+++ src/Login.js
@@ -1,19 +1,16 @@
 import React,{useEffect,useState} from 'react';
 import {View, StyleSheet, Text, TouchableOpacity, TextInput} from 'react-native';
-import * as Font from "expo-font"
-
-const getFonts = ()=> Font.loadAsync({
-    'nunito-regular':require(`../assets/fonts/Nunito-Regular.ttf`),
-    'nunito-bold':require(`../assets/fonts/Nunito-SemiBold.ttf`)
-});
 
 const Login = () => {
-    const[fontsLoaded,setFontsLoaded] = useState(false);
-
-    if(fontsLoaded)
     return (
         <View style={styles.container}>
             <Text style={styles.title}>Login</Text>
+            <TextInput style={styles.input} placeholder="Email"/>
+            <TextInput style={styles.input} placeholder="Password" secureTextEntry={true}/>
+            <Text style={styles.forgot}>Forgot Password ?</Text>
+            <TouchableOpacity style={styles.login}><Text style={styles.text}>Login</Text></TouchableOpacity>
+            <Text style={styles.or}>Or</Text>
+            <TouchableOpacity style={styles.create}><Text style={styles.text}>Create an Account</Text></TouchableOpacity>
         </View>
     );
 }
@@ -24,10 +21,65 @@ const styles = StyleSheet.create({
         flexDirection:"column",
         alignItems:"center",
         backgroundColor:"#000000",
+        padding:30,
+        fontFamily:"nunito-regular",
     },
     title:{
         color:"#ffffff",
-        fontSize:36,
+        fontSize:40,
+        fontFamily:"nunito-bold",
+        marginTop:80,
+        marginBottom:60,
+    },
+    input:{
+        height:55,
+        width:320,
+        borderRadius:30,
+        backgroundColor:"#171717",
+        padding:15,
+        marginBottom:10,
+        color:"white",
+        fontFamily:"nunito-regular",
+        fontSize:20,
+        marginTop:15,
+    }, 
+    forgot:{
+        color:"#353535",
+        fontSize:13,
+        alignSelf:"flex-end",
+        fontFamily:"nunito-regular",
+    },
+    login:{
+        height:50,
+        width:320,
+        borderRadius:30,
+        backgroundColor:"#C7ED39",
+        padding:12,
+        marginTop:10,
+        marginBottom:10,
+        alignItems:"center",
+        justifyContent:"center",
+        marginTop:150,
+    },
+    create:{
+        height:50,
+        width:320,
+        borderRadius:30,
+        backgroundColor:"#FFFFFF",
+        padding:12,
+        marginTop:10,
+        marginBottom:10,
+        alignItems:"center",
+        justifyContent:"center",
+    },
+    text:{
+        fontSize:17,
+        fontFamily:"nunito-bold",
+    },
+    or:{
+        color:"#353535",
+        fontSize:16,
+        fontFamily:"nunito-regular",
     }
 })
 
